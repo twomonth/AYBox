7e8606f5d22cfda64b8b6a543ac1d3eb1f9b63db
twomonth <929353463@qq.com>
2017-10-11 15:38:06 +0800

    2017/09/26 礼包领取页

--- app/src/main/java/com/aygames/twomonth/aybox/activity/LibaoActivity.java
+++ app/src/main/java/com/aygames/twomonth/aybox/activity/LibaoActivity.java
@@ -1,8 +1,17 @@
 package com.aygames.twomonth.aybox.activity;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by wf05 on 2017/9/25.
  */
 
-public class LibaoActivity {
+public class LibaoActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView();
+    }
 }
